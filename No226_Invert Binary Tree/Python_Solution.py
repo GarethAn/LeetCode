db151e14364f38449f75155fae1f50b0fbb15b43
Gareth <garethhhh.au@gmail.com>
2017-06-28 14:31:57 +1000

    Create Python_Solution.py

---
+++ No226_Invert Binary Tree/Python_Solution.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Renyi Hou.  23/6/2017
+题目：
+Invert a binary tree.
+
+解题思路:
+基本的思路是递归
+
+解答：
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :python self的意思是类的实例 (the instance of class)
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        if root.left:
+            self.invertTree(root.left)
+        if root.right:
+            self.invertTree(root.right)
+        root.left, root.right = root.right,root.left
+        # above is a standard method that swap two variable values in Python
+        return root
+        
+Tips:
+Python语言当中，self表示instance of the class
+在class Solution中，self表示instance of the Soluction
+所以说如果要调用Solution中的invertTree，需要self.inverTree(argument)
